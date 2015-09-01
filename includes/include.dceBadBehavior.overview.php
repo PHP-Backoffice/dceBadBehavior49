3dcd16abce439a25480f14a83cdf5417b69b22c0
Ortwin Pinke <dceonline@users.noreply.github.com>
2015-09-01 21:56:55 +0000

    updates

--- includes/include.dceBadBehavior.overview.php
+++ includes/include.dceBadBehavior.overview.php
@@ -18,7 +18,11 @@ defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization 
 // we use our own jquery version
 $cfg['backend_template']['js_files'][0] = "{basePath}plugins/dceBadBehavior/scripts/jquery.min.js";
 
+$aCfgBB = cRegistry::getConfigValue('plugins', 'dceBadBehavior');
+
 $oPage = new dceBadBehaviorGui("dceBadBehavior_overview", "dceBadBehavior", 0);
+$oPage->set('s', 'INFO_URL', 'http://php-backoffice.de');
+$oPage->set('s', 'DOKU_URL', 'http://php-backoffice.de');
 $oPage->addStyle($oPage->getAbsHtmlPath()."libs/DataTables/datatables.min.css");
 $oPage->addScript($oPage->getAbsHtmlPath()."libs/DataTables/datatables.min.js");
 $oPage->addScript($oPage->getAbsHtmlPath()."libs/DataTables/Buttons-1.0.1/js/buttons.flash.min.js");
@@ -28,8 +32,27 @@ $oPage->addScript($oPage->getAbsHtmlPath()."scripts/jquery.plainmodal.min.js");
 
 // set labels and content
 $oPage->set('s', 'LOG_SECTION_H3_HTML', i18n("Bad Behavior Log", "dceBadBehavior"));
-
+$oPage->set('s', 'TXT_MORE_INFO', i18n("more Informations", "dceBadBehavior"));
+$oPage->set('s', 'TXT_TH_0', i18n("ID", "dceBadBehavior"));
+$oPage->set('s', 'TXT_TH_1', i18n("Key", "dceBadBehavior"));
+$oPage->set('s', 'TXT_TH_2', i18n("Date", "dceBadBehavior"));
+$oPage->set('s', 'TXT_TH_3', i18n("Agent", "dceBadBehavior"));
 $oPage->set('s', 'OPTIONS_SECTION_H3_HTML', i18n("Options", "dceBadBehavior"));
+$oPage->set('s', 'TXT_LABEL_CLIENT', i18n("Activate BB for current Client", "dceBadBehavior"));
+$oPage->set('s', 'TXT_LABEL_LOG', i18n("enable Log", "dceBadBehavior"));
+$oPage->set('s', 'TXT_LABEL_VERBOSE', i18n("for all visits", "dceBadBehavior"));
+
+if($aCfgBB['settings']['logging'] === TRUE) {
+    $oPage->set('s', 'INPUT_LOG_ACTIVE_SELECTED', ' checked="checked"');
+} else {
+    $oPage->set('s', 'INPUT_LOG_ACTIVE_SELECTED', "");
+}
+
+if($aCfgBB['settings']['verbose'] === TRUE) {
+    $oPage->set('s', 'INPUT_LOG_VERBOSE_SELECTED', ' checked="checked"');
+} else {
+    $oPage->set('s', 'INPUT_LOG_VERBOSE_SELECTED', "");
+}
 
 $oPage->render();
 ?>
